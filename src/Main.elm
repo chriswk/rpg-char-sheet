4f9238eb48fd4dcb1e54019ad2f5a858f75e3e9f
Christopher Kolstad <christopher.kolstad@nav.no>
2020-01-20 14:16:35 +0100

    Handles skill points

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,17 @@ module Main exposing (..)
 import Browser as B
 import Html exposing (..)
 import Html.Attributes as A
+import Html.Events as E
 
 
 type Msg
     = NoOp
+    | ChangeStrength String
+    | ChangeDexterity String
+    | ChangeConstitution String
+    | ChangeWisdom String
+    | ChangeIntelligence String
+    | ChangeCharisma String
 
 
 type alias Skills =
@@ -46,6 +53,114 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
+        ChangeStrength newStr ->
+            let
+                str =
+                    String.toInt newStr
+
+                skills =
+                    model.skills
+
+                newS =
+                    case str of
+                        Just s ->
+                            { skills | strength = s }
+
+                        Nothing ->
+                            skills
+            in
+            ( { model | skills = newS }, Cmd.none )
+
+        ChangeDexterity newDex ->
+            let
+                dex =
+                    String.toInt newDex
+
+                skills =
+                    model.skills
+
+                newS =
+                    case dex of
+                        Just d ->
+                            { skills | dexterity = d }
+
+                        Nothing ->
+                            skills
+            in
+            ( { model | skills = newS }, Cmd.none )
+
+        ChangeConstitution newCon ->
+            let
+                con =
+                    String.toInt newCon
+
+                skills =
+                    model.skills
+
+                newS =
+                    case con of
+                        Just d ->
+                            { skills | constitution = d }
+
+                        Nothing ->
+                            skills
+            in
+            ( { model | skills = newS }, Cmd.none )
+
+        ChangeWisdom newWis ->
+            let
+                wis =
+                    String.toInt newWis
+
+                skills =
+                    model.skills
+
+                newS =
+                    case wis of
+                        Just d ->
+                            { skills | wisdom = d }
+
+                        Nothing ->
+                            skills
+            in
+            ( { model | skills = newS }, Cmd.none )
+
+        ChangeIntelligence newInt ->
+            let
+                int =
+                    String.toInt newInt
+
+                skills =
+                    model.skills
+
+                newS =
+                    case int of
+                        Just d ->
+                            { skills | intelligence = d }
+
+                        Nothing ->
+                            skills
+            in
+            ( { model | skills = newS }, Cmd.none )
+
+        ChangeCharisma newCha ->
+            let
+                cha =
+                    String.toInt newCha
+
+                skills =
+                    model.skills
+
+                newS =
+                    case cha of
+                        Just d ->
+                            { skills | charisma = d }
+
+                        Nothing ->
+                            skills
+            in
+            ( { model | skills = newS }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -113,6 +228,13 @@ inputText name labelText placeholder =
     ]
 
 
+textArea : String -> String -> List (Html Msg)
+textArea name labelText =
+    [ label [ A.for name ] [ text labelText ]
+    , textarea [ A.name name ] []
+    ]
+
+
 miscList : Model -> List (Html Msg)
 miscList model =
     [ ul []
@@ -137,6 +259,104 @@ mainBody model =
 
 skillsAndProficiencies : Model -> List (Html Msg)
 skillsAndProficiencies model =
+    [ section [ A.class "attributes" ] (attributesList model)
+    , div [ A.class "passive-perception", A.class "box" ] (passivePerception model)
+    , div [ A.class "otherprefs", A.class "box", A.class "textblock" ] (textArea "otherprofs" "Other Proficiencies and Languages")
+    ]
+
+
+applications : Model -> Html Msg
+applications model =
+    div [ A.class "attr-applications" ] []
+
+
+attributesList : Model -> List (Html Msg)
+attributesList model =
+    [ scores model
+    , applications model
+    ]
+
+
+scores : Model -> Html Msg
+scores model =
+    div [ A.class "scores" ]
+        [ ul []
+            [ li []
+                [ div [ A.class "score" ]
+                    [ label [ A.for "Strengthscore" ] [ text "Strength" ]
+                    , input [ A.type_ "number", A.min "0", A.max "25", E.onInput ChangeStrength, A.name "Strengthscore", A.value (String.fromInt model.skills.strength) ] []
+                    ]
+                , div [ A.class "modifier" ]
+                    [ input [ A.name "Strengthmod", A.placeholder (modifier model.skills.strength) ] []
+                    ]
+                ]
+            , li []
+                [ div [ A.class "score" ]
+                    [ label [ A.for "Dexterityscore" ] [ text "Dexterity" ]
+                    , input [ A.type_ "number", A.min "0", A.max "25", E.onInput ChangeDexterity, A.name "Dexterityscore", A.value (String.fromInt model.skills.dexterity) ] []
+                    ]
+                , div [ A.class "modifier" ]
+                    [ input [ A.name "Dexteritymod", A.placeholder (modifier model.skills.dexterity) ] []
+                    ]
+                ]
+            , li []
+                [ div [ A.class "score" ]
+                    [ label [ A.for "Constitutionscore" ] [ text "Constitution" ]
+                    , input [ A.type_ "number", A.min "0", A.max "25", E.onInput ChangeConstitution, A.name "Constitutionscore", A.value (String.fromInt model.skills.constitution) ] []
+                    ]
+                , div [ A.class "modifier" ]
+                    [ input [ A.name "Constitutionmod", A.placeholder (modifier model.skills.constitution) ] []
+                    ]
+                ]
+            , li []
+                [ div [ A.class "score" ]
+                    [ label [ A.for "Wisdomscore" ] [ text "Wisdom" ]
+                    , input [ A.type_ "number", A.min "0", A.max "25", E.onInput ChangeWisdom, A.name "Wisdomscore", A.value (String.fromInt model.skills.wisdom) ] []
+                    ]
+                , div [ A.class "modifier" ]
+                    [ input [ A.name "Wisdommod", A.placeholder (modifier model.skills.wisdom) ] []
+                    ]
+                ]
+            , li []
+                [ div [ A.class "score" ]
+                    [ label [ A.for "Intelligencescore" ] [ text "Intelligence" ]
+                    , input [ A.type_ "number", A.min "0", A.max "25", E.onInput ChangeIntelligence, A.name "Intelligencescore", A.value (String.fromInt model.skills.intelligence) ] []
+                    ]
+                , div [ A.class "modifier" ]
+                    [ input [ A.name "Intelligencemod", A.placeholder (modifier model.skills.intelligence) ] []
+                    ]
+                ]
+            , li []
+                [ div [ A.class "score" ]
+                    [ label [ A.for "Charismascore" ] [ text "Charisma" ]
+                    , input [ A.type_ "number", A.min "0", A.max "25", E.onInput ChangeCharisma, A.name "Charismascore", A.value (String.fromInt model.skills.charisma) ] []
+                    ]
+                , div [ A.class "modifier" ]
+                    [ input [ A.name "Charismamod", A.placeholder (modifier model.skills.charisma) ] []
+                    ]
+                ]
+            ]
+        ]
+
+
+modifier : Int -> String
+modifier skill =
+    let
+        mod =
+            (skill - 10) // 2
+
+        symbol =
+            if mod < 0 then
+                ""
+
+            else
+                "+"
+    in
+    symbol ++ String.fromInt mod
+
+
+passivePerception : Model -> List (Html Msg)
+passivePerception model =
     []
 
 
